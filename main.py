e82dbcfede906fc69721e6c62bfdfa6a66166252
WRICHY01 <goddyrichard01@gmail.com>
2026-09-06 00:59:55 +0100

    feat: complete registration flow with full validation tests

--- main.py
+++ main.py
@@ -1,20 +1,69 @@
 from fastapi import FastAPI, HTTPException
-import sys
+from pydantic import BaseModel, EmailStr
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
 
 app = FastAPI()
+ph = PasswordHasher()
 
-@app.get("/")
-def read_root():
+MIN_PASSWORD_LENGTH = 8
+users_db = {}
+
+class UserRegisteration(BaseModel):
+    email: EmailStr
+    password: str
+
+@app.get('/')
+def get_root():
     """
-    Home page endpoint.
-    Serves a simple welcome message in JSON format.
+    root directory of the application
     """
-    return {"message": "Welcome to the Authentication Core."}
+    return {"message": "Welcome to the homepage!"}
 
 @app.get("/health")
-def health_check():
+def get_health_status():
     """
-    Health check endpoint.
-    Indicates that the app is running smoothly and ready for traffic.
+    The health of the site
     """
     return {"status": "ok"}
+
+@app.post("/signup")
+def register_user(user_cred: UserRegisteration):
+    """
+    Validate the user's credentials on sign up before saving to the user-database.
+    """
+    if user_cred.email in users_db:
+        raise HTTPException(status_code=400, detail="That email is already in use!")
+
+    if not user_cred.password:
+        raise HTTPException(status_code=400, detail="Password cannot be empty!")
+    
+    if len(user_cred.password) < MIN_PASSWORD_LENGTH: # why does int not have len method or whats it equivalence?
+        raise HTTPException(status_code=400, detail=f"Password must have a min of {MIN_PASSWORD_LENGTH} characters long")
+
+    hashed_user_password = ph.hash(user_cred.password)
+    users_db[user_cred.email] = hashed_user_password
+
+    return {"message": "Your credentials have been saved! you can now log in"}
+
+
+@app.post("/signin")
+def user_signin(user_cred: UserRegisteration):
+    """
+    validate the user's signin credentials matches information in the user-database.
+    """
+    if user_cred.email not in users_db:
+        raise HTTPException(status_code=400, detail="Email or Password doesnt match")
+    
+    if not user_cred.password:
+        raise HTTPException(status_code=400, detail="Password cannot be empty!")
+    
+    try:
+        ph.verify(users_db[user_cred.email], user_cred.password)
+        return {"message": "Welcome back!"}
+    except VerifyMismatchError:
+        raise HTTPException(status_code=400, detail="Email or Password doesnt match")
+    
+
+
--- test_main.py
+++ test_main.py
@@ -1,24 +1,69 @@
 from fastapi.testclient import TestClient
-from main import app
+from main import app, MIN_PASSWORD_LENGTH
+
 
 client = TestClient(app)
 
 def test_read_root():
     """
-    Verifies that the home endpoint returns a 200 status code and
-    the correct welcom message payload.
+    Verifies that the home endpoint is working
     """
 
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the Authentication Core."}
-
+    assert response.json() == {"message": "Welcome to the homepage!"}
 
 def test_health_check():
     """
-    Verifies that the health check endpoint returns a 200 status code and
-    a clean operational status.
+    Verifies that at health endpoint is working as expected
     """
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+def test_signup_flows():
+    """
+    Verifies that the /signup endpoint validates data inputs correctly.
+    """
+    response = client.post("/signup", json={"email": "eoihdgmaicom", "password": "12345678" })
+    assert response.status_code == 422
+
+    response = client.post(url="/signup", json={"email": "eoihd@gmai.com", "password": "" })
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Password cannot be empty!"}
+
+    response = client.post(url="/signup", json={"email": "eoihd@gmai.com", "password": "1234" })
+    assert response.status_code == 400
+    assert response.json() == {"detail": f"Password must have a min of {MIN_PASSWORD_LENGTH} characters long"}
+
+    response = client.post("/signup", json={"email": "eoihd@gmai.com", "password": "supersecure12"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Your credentials have been saved! you can now log in"}
+
+    response = client.post("/signup", json={"email": "eoihd@gmai.com", "password": "supersecure12"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "That email is already in use!"}
+
+
+def test_signin_flows():
+    """
+    Verifies that the /signin endpoint validates data inputs correctly.
+    """
+    response = client.post("/signin", json={"email": "eoihdgmaicom", "password": "12345678" })
+    assert response.status_code == 422
+    
+    response = client.post(url="/signin", json={"email": "eoihd@gmai.com", "password": ""})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Password cannot be empty!"}
+
+    response = client.post(url="/signin", content='{"email": "geoihd@gmai.com", "password": "123456578"}')
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Email or Password doesnt match"}
+
+    response = client.post(url="/signin", json={"email": "eoihd@gmai.com", "password": "supersecure123"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Email or Password doesnt match"}
+
+    response = client.post("/signin", json={"email": "eoihd@gmai.com", "password": "supersecure12"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome back!"}
